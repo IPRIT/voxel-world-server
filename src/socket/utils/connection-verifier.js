ef64a85a5c0d6ffdb815e03f140f3a5da99857af
IPRIT <ipritoflex@yandex.ru>
2018-08-07 23:41:50 +0300

    Update

--- src/socket/utils/connection-verifier.js
+++ src/socket/utils/connection-verifier.js
@@ -1,6 +1,8 @@
 import request from 'request-promise';
 import { config } from "../../../config";
 
+const defaultErrorCode = 'invalid_game_session';
+
 /**
  * @param {*} request
  * @param {Function} next
@@ -8,20 +10,14 @@ import { config } from "../../../config";
 export function connectionVerifier ({ handshake = {} }, next) {
   const { query = {} } = handshake;
   const { gameToken } = query;
-  const defaultErrorCode = 'invalid_game_session';
 
   if (!gameToken) {
     return next( new Error( defaultErrorCode ) );
   }
 
-  return verifyToken( gameToken ).then(_ => {
-    console.log( `[Socket#connection] game token (${gameToken.slice(0, 10)}) verified by server!` );
-    next();
-  }).catch(({ error = {} } = {}) => {
-    console.log( `[Socket#connection] game token (${gameToken.slice(0, 10)}) declined by server.` );
-    const serverError = error.error || {};
-    next( new Error( serverError.code || defaultErrorCode ) );
-  });
+  return verifyToken( gameToken )
+    .then(_ => next())
+    .catch( next );
 }
 
 /**
@@ -30,8 +26,16 @@ export function connectionVerifier ({ handshake = {} }, next) {
  * @return {Promise<boolean>}
  */
 export async function verifyToken (gameToken, serverOptions = {}) {
-  console.log( `[Socket#connection] verifying game token (${gameToken.slice(0, 10)})...` );
-  return fetchGameSession( gameToken, serverOptions );
+  console.log( `[Socket#connection] verifying game token (${gameToken.slice(0, 10)}...).` );
+
+  return fetchGameSession( gameToken, serverOptions ).then(session => {
+    console.log( `[Socket#connection] game token (${gameToken.slice(0, 10)}...) verified by server!` );
+    return session;
+  }).catch(({ error = {} } = {}) => {
+    console.log( `[Socket#connection] game token (${gameToken.slice(0, 10)}...) declined by server.` );
+    const serverError = error.error || {};
+    throw new Error( serverError.code || defaultErrorCode );
+  });
 }
 
 /**
